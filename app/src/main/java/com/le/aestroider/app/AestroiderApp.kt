7bdead5afac3bd617ecdc033ed2a8815596f5db4
Usman <usman@laundrapp.com>
2019-01-11 21:11:26 +0000

    Inits di

--- app/src/main/java/com/le/aestroider/app/AestroiderApp.kt
+++ app/src/main/java/com/le/aestroider/app/AestroiderApp.kt
@@ -1,15 +1,21 @@
 package com.le.aestroider.app
 
 import android.app.Application
+import com.le.aestroider.di.AppModule
+import com.le.aestroider.di.DaggerDataComponent
+import com.le.aestroider.di.DataComponent
+import com.le.aestroider.di.DataModule
 
 class AestroiderApp : Application() {
 
     companion object {
         lateinit var aestroiderApp: AestroiderApp
+        lateinit var dataComponent : DataComponent
     }
 
     override fun onCreate() {
         super.onCreate()
         aestroiderApp = this
+        dataComponent = DaggerDataComponent.builder().appModule(AppModule(this)).dataModule(DataModule()).build()
     }
 }
