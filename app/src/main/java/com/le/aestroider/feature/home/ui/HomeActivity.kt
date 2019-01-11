ef4566b71f0fca894a6aacc8d8ef21df70a04e73
Usman <usman@laundrapp.com>
2019-01-11 18:44:35 +0000

    First commit. Contains project basic skeleton

---
+++ app/src/main/java/com/le/aestroider/feature/home/ui/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.le.aestroider.feature.home.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.le.aestroider.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
