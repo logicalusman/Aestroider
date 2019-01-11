c7cc3dba11d37b70ee7260fde91e5d6e8e7765ff
Usman <usman@laundrapp.com>
2019-01-11 23:34:57 +0000

    Acts as a static class to get access to network related objects and consts

---
+++ app/src/main/java/com/le/aestroider/data/network/NetworkAdapter.kt
@@ -0,0 +1,32 @@
+package com.le.aestroider.data.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkAdapter {
+
+    const val API_AUTH_KEY = "mO4kPg8EJvLCxsMAj9kW2igX5lJwuFSOn3BIIQuy"
+    const val BASE_URL = "https://api.nasa.gov"
+    const val GET_NEO_FEED = "/neo/rest/v1/feed"
+
+
+    fun nasaNeoApi(): NasaNeoApi {
+        val builder = OkHttpClient.Builder()
+        builder.addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+
+        val retrofit = Retrofit.Builder()
+            .addCallAdapterFactory(
+                RxJava2CallAdapterFactory.create())
+            .addConverterFactory(
+                GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(builder.build())
+            .build()
+
+        return retrofit.create(NasaNeoApi::class.java)
+    }
+
+}
