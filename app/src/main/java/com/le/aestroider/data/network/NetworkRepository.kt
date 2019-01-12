2538ff8bcb288a200837a7cff828e436042b4b27
Usman <usman@laundrapp.com>
2019-01-12 16:49:39 +0000

    Sorts the neo objects map date wise

--- app/src/main/java/com/le/aestroider/data/network/NetworkRepository.kt
+++ app/src/main/java/com/le/aestroider/data/network/NetworkRepository.kt
@@ -2,12 +2,13 @@ package com.le.aestroider.data.network
 
 import android.util.Log
 import com.le.aestroider.data.base.BaseRepository
+import com.le.aestroider.util.Utils
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 /**
- * Performs data operation over the network. This class is private to data layer - viewmodel(s) should never access this
+ * Performs data operation over the network. This class is internal to data layer - viewmodel(s) should never access this
  * class.
  *
  * @author Usman
@@ -18,8 +19,14 @@ class NetworkRepository @Inject constructor(private val neoApi: NasaNeoApi) : Ba
 
     fun getNeoFeed() {
 
-        neoApi.getMyPriorities(startDate = "2018-01-11", endDate = "2018-01-18").subscribeOn(Schedulers.io()).map {
+        neoApi.getMyPriorities(startDate = "2018-01-12", endDate = "2018-01-19").subscribeOn(Schedulers.io()).map {
+
+            val sortedMap = it.neoFeed.toSortedMap(Utils.dateComparator())
+
             Log.d(TAG, "Number of objects: ${it.numNeoObjects}")
+            Log.d(TAG, it.pageLinks.next)
+            Log.d(TAG, "Neo feed size: ${sortedMap.keys}")
+
         }
             .observeOn(AndroidSchedulers.mainThread()).subscribe()
 
