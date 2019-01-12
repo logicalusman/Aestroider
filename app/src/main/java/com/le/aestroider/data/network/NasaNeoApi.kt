e8918adaf75584446a96b86e231e2ab7a7487437
Usman <usman@laundrapp.com>
2019-01-12 21:30:46 +0000

    Name change

--- app/src/main/java/com/le/aestroider/data/network/NasaNeoApi.kt
+++ app/src/main/java/com/le/aestroider/data/network/NasaNeoApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface NasaNeoApi {
 
     @GET(NetworkAdapter.GET_NEO_FEED)
-    fun getMyPriorities(
+    fun getNeoFeed(
         @Query(value = "start_date") startDate: String,
         @Query(value = "end_date") endDate: String,
         @Query(value = "api_key") apiKey: String = NetworkAdapter.API_AUTH_KEY,
