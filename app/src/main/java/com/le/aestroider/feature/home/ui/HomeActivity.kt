4f27ef431d084c00579b0a192ff1d7603d98141e
Usman <usman@laundrapp.com>
2019-01-13 12:44:42 +0000

    Activity now contains fragment tag directly

--- app/src/main/java/com/le/aestroider/feature/home/ui/HomeActivity.kt
+++ app/src/main/java/com/le/aestroider/feature/home/ui/HomeActivity.kt
@@ -2,7 +2,6 @@ package com.le.aestroider.feature.home.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.le.aestroider.R
 
 class HomeActivity : AppCompatActivity() {
@@ -10,7 +9,5 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val fragment: Fragment = HomeFragment.newInstance()
-        supportFragmentManager.beginTransaction().add(R.id.home_fragment_container, fragment).commit()
     }
 }
