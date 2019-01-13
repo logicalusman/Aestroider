c40b7d171a3685a8f469cc73c3625082604d125b
Usman <usman@laundrapp.com>
2019-01-13 13:15:09 +0000

    Added ui for showing neo details

---
+++ app/src/main/java/com/le/aestroider/feature/neodetails/ui/NeoDeailsFragment.kt
@@ -0,0 +1,30 @@
+package com.le.aestroider.feature.neodetails.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.le.aestroider.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class NeoDeailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_neo_deails, container, false)
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/le/aestroider/feature/neodetails/ui/NeoDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.le.aestroider.feature.neodetails.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.le.aestroider.R
+
+class NeoDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_neo_details)
+    }
+}
