b0d6810473b5c3aff398eaf4b5852c31ed062067
Usman <usman@laundrapp.com>
2019-01-13 15:04:11 +0000

    Implemented back key click

--- app/src/main/java/com/le/aestroider/feature/neodetails/ui/NeoDetailsActivity.kt
+++ app/src/main/java/com/le/aestroider/feature/neodetails/ui/NeoDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.le.aestroider.feature.neodetails.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.le.aestroider.R
@@ -14,11 +15,19 @@ class NeoDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_neo_details)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val fragment: Fragment = NeoDetailsFragment.newInstance(
             intent.getParcelableExtra(
                 EXTRA_NEO_DETAILS
             )
         )
         supportFragmentManager.beginTransaction().add(R.id.neo_details_fragment_container, fragment).commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
