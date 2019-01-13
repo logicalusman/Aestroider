eb7e2673647ccbad1c155c343869113783247117
Usman <usman@laundrapp.com>
2019-01-13 19:29:24 +0000

    Implemented neo details ui.
    Shows single piece of neo info on a separate row on list

--- app/src/main/java/com/le/aestroider/feature/neodetails/ui/NeoDetailsFragment.kt
+++ app/src/main/java/com/le/aestroider/feature/neodetails/ui/NeoDetailsFragment.kt
@@ -1,19 +1,27 @@
 package com.le.aestroider.feature.neodetails.ui
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.le.aestroider.R
 import com.le.aestroider.domain.NearEarthObject
+import com.le.aestroider.domain.NearEarthObjectDetailsItem
+import com.le.aestroider.feature.neodetails.adapter.NeoDetailsAdapter
 import com.le.aestroider.feature.neodetails.viewmodel.NeoDetailsViewModel
+import kotlinx.android.synthetic.main.fragment_neo_details.*
 
 
 /**
- * A simple [Fragment] subclass.
+ * Show Neo (Near Earth Object) details
  *
  */
 class NeoDetailsFragment : Fragment() {
@@ -22,6 +30,7 @@ class NeoDetailsFragment : Fragment() {
 
     // private vars
     private lateinit var viewModel: NeoDetailsViewModel
+    private var neoDetailsAdapter: NeoDetailsAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,8 +44,41 @@ class NeoDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        subscribeViewStates()
+        viewModel.init(nearEarthObject)
     }
 
+    private fun subscribeViewStates() {
+        viewModel.viewState.observe(activity!!, Observer {
+            when (it) {
+                is NeoDetailsViewModel.ViewState.UpdateTitle -> activity!!.title = it.title
+                is NeoDetailsViewModel.ViewState.UpdateList -> updateNeoFeedDetails(it.list)
+                is NeoDetailsViewModel.ViewState.LaunchBrowser -> launchBrowser(it.uri)
+            }
+        })
+    }
+
+    private fun updateNeoFeedDetails(list: List<NearEarthObjectDetailsItem>) {
+        neoDetailsAdapter?.listItems = list
+    }
+
+    private fun launchBrowser(uri: String) {
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(uri)
+        startActivity(i)
+    }
+
+    private fun setupViews() {
+        val layoutManager = LinearLayoutManager(activity)
+        neo_details_rv.layoutManager = layoutManager
+        neo_details_rv.addItemDecoration(DividerItemDecoration(activity, layoutManager.orientation))
+        neoDetailsAdapter = NeoDetailsAdapter(activity!!)
+        neo_details_rv.adapter = neoDetailsAdapter
+        url_btn.setOnClickListener {
+            viewModel.onOpenUriButtonSelected()
+        }
+    }
 
     companion object {
 
