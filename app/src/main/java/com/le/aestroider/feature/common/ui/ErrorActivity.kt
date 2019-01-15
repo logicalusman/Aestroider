1d68d07258d0432ddf32ebb99ac0a350b0d58ed9
Usman <usman@laundrapp.com>
2019-01-15 13:05:54 +0000

    Added error screen

---
+++ app/src/main/java/com/le/aestroider/feature/common/ui/ErrorActivity.kt
@@ -0,0 +1,13 @@
+package com.le.aestroider.feature.common.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.le.aestroider.R
+
+class ErrorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error)
+    }
+}
